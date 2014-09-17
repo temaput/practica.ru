91df94fa6148e0da10da46031dfa615f7c6e5906
temaput <putilkin@gmail.com>
2014-09-17 18:40:57 +0400

    sorting on homepage

--- practica/apps/promotions/views.py
+++ practica/apps/promotions/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.conf import settings 
+from django.conf import settings
 from django.db.models import get_model
 
 Category = get_model('catalogue', 'Category')
 
 # Create your views here.
+
 
 class PracticaHomeView(ListView):
     """
@@ -16,4 +16,5 @@ class PracticaHomeView(ListView):
 
     def get_queryset(self):
         return Category.objects.get(
-                slug=settings.HOME_PAGE_CATEGORY_SLUG).product_set.all()
+            slug=settings.HOME_PAGE_CATEGORY_SLUG).product_set.order_by(
+            "title")
