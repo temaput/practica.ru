a19310c8327531bf56f3a3d3feb4ace13cef9d15
tema <putilkin@gmail.com>
2015-09-08 09:36:12 +0300

    email backend

--- practica/practica/settings/production.py
+++ practica/practica/settings/production.py
@@ -61,8 +61,8 @@ THUMBNAIL_REDIS_DB = 1  # redis provides up to 16 dbs by default
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.nic.ru'
-EMAIL_HOST_USER = os.environ['PRACTICA_EMAIL']
-EMAIL_HOST_PASSWORD = os.environ['PRACTICA_EMAIL_PASSWORD']
+EMAIL_HOST_USER = get_env('PRACTICA_EMAIL')
+EMAIL_HOST_PASSWORD = get_env('PRACTICA_EMAIL_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_PORT = '465'
 SERVER_EMAIL = EMAIL_HOST_USER
