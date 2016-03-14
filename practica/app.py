249e3b3798aa09ca4d2e9147340302d41a4ccb18
Artem Putilov <putilkin@gmail.com>
2016-03-14 12:15:44 +0300

    add sbrfslip

--- practica/app.py
+++ practica/app.py
@@ -5,6 +5,7 @@ from oscar.app import Shop
 from apps.checkout.app import application as checkout_app
 from apps.promotions.app import application as promo_app
 from apps.dashboard.app import application as dashboard_app
+from apps.invoice.app import application as invoice_app
 
 
 class Main(Shop):
@@ -12,11 +13,12 @@ class Main(Shop):
     checkout_app = checkout_app
     promotions_app = promo_app
     dashboard_app = dashboard_app
+    invoice_app = invoice_app
 
     def get_urls(self):
         urlpatterns = super(Main, self).get_urls()
-        # urlpatterns += patterns('',
-        #        url(r"^invoice/", include(self.invoice_app.urls)))
+        urlpatterns += patterns('',
+        url(r"^invoice/", include(self.invoice_app.urls)))
         urlpatterns += patterns(
             '',
             url(r"^[Bb]ooks/", include('oldsite_swapper.urls'))
--- practica/practica/requisites.py
+++ practica/practica/requisites.py
@@ -5,18 +5,15 @@ entities = dict(
     raquo = u'\u00BB')
 
 REQUISITES = dict(
-            name = u"ООО {laquo}Издательство ГРАНАТ{raquo}".format(
-                **entities),
-            INN = "7729707288",
-            BIK = "044525986",
-            KPP = "772901001",
-            correspondentAccount = "30101810600000000986",
-            beneficiaryAccount = "40702810000010066732",
-            bankName = (u"Расчетный счет ОАО АКБ "
-                u"{laquo}Пробизнесбанк{raquo} г. Москва".format(
-                    **entities)),
+            name = u"ООО «Издательский дом «Практика»",
+            INN = "7705166992",
+            BIK = "044525225",
+            KPP = "",
+            correspondentAccount = "30101810400000000225",
+            beneficiaryAccount = "40702810138040103580",
+            bankName = (u"Сбербанк России ОАО, г. Москва, Московский банк Сбербанка России ОАО"),
             address=(u"121471, Москва г, Рябиновая ул, дом №44, пом.1, "
                 u"тел. (499) 391-48-04"),
-            manager = u"Рябцева Мария Александровна",
-            accountant = u"Анурова Ольга Львовна"
+            manager = u"Ананич Сергей Владимирович",
+            accountant = u"Гохберг Валерия Лазоревна"
             )
